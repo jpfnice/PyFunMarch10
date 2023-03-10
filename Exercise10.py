5fc162fda7053941dafb037c8ea715a5a710cd0a
jpfnice <jpfnice@gmail.com>
2023-03-10 10:53:16 +0100

    Add files via upload

--- Exercise10.py
+++ Exercise10.py
@@ -15,8 +15,42 @@ Methods:
     __eq__()
 """
 
-class Stack:
-    pass
+class Stack: 
+    def __init__(self, size):
+        if isinstance(size, int) and size > 0:
+            self.maxi=size
+            self.data=[]
+        else:
+            print("Wrong size given")
+            
+    def __repr__(self): # (3/10) [20,56,45]
+        
+        return f"({len(self.data)}/{self.maxi}) {self.data}"
+    
+    def __eq__(self, other):
+        
+        return self.maxi == other.maxi and self.data == other.data
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def push(self, element):
+        if len(self) < self.maxi:
+            self.data.append(element)
+        else:
+            print("The Stack is Full!")
+            
+    def pop(self):
+        if len(self)>0:
+            return self.data.pop()
+        else:
+            print("The Stack is Empty!")
+            
+    def peek(self):
+        if len(self)>0:
+            return self.data[-1]
+        else:
+            print("The Stack is Empty!")
 
 s1=Stack(10) # a new Stack with a max size of 10 elements
 s1.push(20)
